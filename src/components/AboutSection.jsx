391041776972347dfb7c0bf7d17fafef94dcb034
Divyanshgarg-04 <divyanshgarg955@gmail.com>
2024-07-01 18:43:57 +0530

    UI fix for About and Project Page

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import image from '../assets/image.avif';
-import Skills from './Skills';
+// import Skills from './Skills';
 
 function AboutSection() {
     return (
-        <div className='w-full h-8/9 flex flex-row justify-center'>
+        <div className='w-full h-8/9 flex flex-col sm:flex-row justify-center'>
+            {/* Right Part */}
+            <div className='h-1/2 w-[80%] mt-10 sm:h-[80%] sm:w-[40%] flex flex-row justify-end ml-10 sm:ml-10 sm:mt-20 '>
+                <img src={image} alt="my-image" className='h-full rounded-2xl' />
+            </div>
+
             {/* Left Part */}
-            <div className='flex justify-between flex-row ml-8 mr-2 p-2 my-14 w-1/2 sm:w-[60%] h-[1/2] sm:h-[55%] '>
+            <div className='flex justify-between flex-row px-6  my-14 w-full sm:w-[60%] h-[1/2] sm:h-[55%] '>
                 <div className='flex flex-col justify-center text-center text-sm sm:text-3xl rounded text-white font-semibold'>
                     <h1 className=''>Hello there! I'm Divyansh, an enthusiastic undergraduate student at NIT Rourkela with a deep passion for technology. I thrive on exploring the latest advancements in this ever-evolving field!
                     </h1>
@@ -19,10 +24,6 @@ function AboutSection() {
                 </div>
             </div>
 
-            {/* Right Part */}
-            <div className='h-1/2 w-1/2 mt-48 sm:h-[80%] sm:w-[40%] flex flex-row justify-end pr-4 sm:mr-10 sm:mt-20 '>
-                <img src={image} alt="my-image" className='h-full rounded-2xl' />
-            </div>
 
         </div>
     )
